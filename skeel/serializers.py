2d338141fc36e339da6f14e7aa946640583f8b7d
Camilord <camilotk@gmail.com>
2019-04-10 14:09:45 -0300

    Created the first Serializer

---
+++ skeel/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import *
+
+class JobVacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobVacancy
+        fields = {"id", "title", "description", "salary", "contract_type", "status"}
