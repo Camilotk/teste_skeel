3c4dd3296dd5cf5b6788857f0315f21e9169423f
Camilord <camilotk@gmail.com>
2019-04-10 23:22:20 -0300

    made update por by json request

--- skeel/urls.py
+++ skeel/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-        url(r'^vagas/nova', CreateJobVacancy.as_view()),
+        url(r'^vagas/nova/$', CreateJobVacancy.as_view()),
+        url(r'^vagas/lista/$', ListJobVacancy.as_view()),
+        url(r'^vagas/lista/(?P<pk>[0-9]+)/$', GetJobByID.as_view()),
 ]
+
--- skeel/views.py
+++ skeel/views.py
@@ -14,6 +14,49 @@ class CreateJobVacancy(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
-            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+            return JsonResponse({"mensagem": "Ocorreu um erro com skeel/views.py/CreateJobVacancy"},
                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class ListJobVacancy(APIView):
+    def post(self, request):
+        try:
+            jobs_list = JobVacancy.objects.all()
+            serializer = JobVacancySerializer(jobs_list, many=True)
+            return Response(serializer.data)
+        except Exception:
+            return JsonResponse({"mensagem": "Ocorreu um erro com skeel/views.py/ListJobVacancy"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class GetJobByID(APIView):
+    def post(self, request, pk):
+        try:
+            if pk <= "0":
+                return JsonResponse({"mensagem": "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            vacancy = JobVacancy.objects.get(pk=pk)
+            serializer = JobVacancySerializer(vacancy)
+            return Response(serializer.data)
+        except JobVacancy.DoesNotExist:
+            return JsonResponse({"mensagem": "A vaga não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({"mensagem": "Ocorreu um erro com skeel/views.py/GetJobByID.post()"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, pk):
+        try:
+            if pk <= "0":
+                return JsonResponse({"mensagem": "O ID deve ser maior que zero."},
+                        status=status.HTTP_400_BAD_REQUEST)
+            vacancy = JobVacancy.objects.get(pk=pk)
+            serializer = JobVacancySerializer(vacancy, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        except JobVacancy.DoesNotExist:
+            return JsonResponse({"mensagem": "Isso non ecziste!"},
+                    status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({"mensagem": "Ocorreu um erro com skeel/views.py/GetJobByID.put())"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
